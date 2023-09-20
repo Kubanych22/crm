a86b1383cba2278cdd60a588e554422458c4a5c9
Kubanych22 <z200707@mail.ru>
2023-09-20 17:39:35 +0600

    Module 5, lesson 6

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@ const modal = document.querySelector('.modal');
 const formContainer = document.querySelector('.modal__container')
 const openModalBtn = document.querySelector('.main__button');
 
-openModalBtn.addEventListener('click', () => {  
+openModalBtn.addEventListener('click', () => {
   modal.classList.remove('close-modal');
   modal.classList.add('open-modal');
 });
 
-formContainer.addEventListener('click', (event) => {
-  event.stopPropagation();
-  const isModalOpen = event.target.closest('.modal__close');
-  if (isModalOpen) {
+modal.addEventListener('click', (event) => {
+  const target = event.target;
+  const closeBtn = target.closest('.modal__close');
+  if (target === modal || closeBtn) {
     modal.classList.remove('open-modal');
     modal.classList.add('close-modal');
   }
 });
 
-modal.addEventListener('click', () => {
-    modal.classList.remove('open-modal');
-    modal.classList.add('close-modal');
-})
+const table = document.querySelector('.table');
+
+const deleteRow = (event) => {
+  const target = event.target;
+  const btnDel = target.closest('.table__button-delete');
+  const delRow = btnDel.closest('tr')
+  const tableBody = delRow.closest('tbody');
+  delRow.remove();
+  console.log('Удаленная строка:');
+  console.log(delRow);
+  const afterDeleteTable = tableBody.closest('table');
+  console.log('Таблица после удаления:');
+  console.log(afterDeleteTable);
+}
+
+table.addEventListener('click', deleteRow);
